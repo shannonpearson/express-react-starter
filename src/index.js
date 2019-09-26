9197b8f7c618a596bb2f25faf4ea0e600a8c78af
shannonpearson <schristinep@gmail.com>
2019-09-26 17:49:28 -0500

    add babel and dotenv

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-const express = require('express');
+import express from 'express';
+
+// import 'dontenv/config'
+// process.env...
 
 const app = express();
 const port = 3000;
 
 app.get('*', (req, res) => {
-  res.send('Hello world!');
+  res.send('Hi');
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
