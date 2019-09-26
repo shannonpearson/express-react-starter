80c3371baeee8d485f0154e558e0c41819403d5e
shannonpearson <schristinep@gmail.com>
2019-09-26 17:44:59 -0500

    add nodemon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
 app.get('*', (req, res) => {
-  res.send('Hello world!')
-})
+  res.send('Hello world!');
+});
 
-app.listen(port, () => console.log(`app listening on port ${port}`))
+app.listen(port, () => console.log(`app listening on port ${port}`));
