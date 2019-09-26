8f566a87193bf1b163c2fb6941cef8008ac7d8a4
shannonpearson <schristinep@gmail.com>
2019-09-26 18:31:39 -0500

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-path-concat */
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  externals: ['fs', 'net'],
+};
