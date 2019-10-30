b4f6e535b964b56bee3551b04872204210132a99
shannonpearson <shannon.pearson@goverb.com>
2019-10-30 11:26:16 -0500

    parcel

---
+++ dev-server.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const proxy = require('http-proxy-middleware');
+const Bundler = require('parcel-bundler');
+const express = require('express');
+
+const bundler = new Bundler('./index.html');
+const app = express();
+
+app.use('/api', proxy({
+  target: 'http://localhost:3000',
+  pathRewrite: {
+    '^/api': '',
+  },
+  changeOrigin: true,
+}));
+
+app.use(bundler.middleware());
+
+app.listen(1234, () => console.log('Server running at http://localhost:1234'));
