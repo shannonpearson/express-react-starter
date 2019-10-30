2a6a548361d523a416f6389c6ee9ddc2e4bcf2b5
shannonpearson <shannon.pearson@goverb.com>
2019-10-30 11:48:37 -0500

    fix user route

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const User = require('./user')
+
 const connectDb = () => mongoose.connect(process.env.DATABASE_URL);
 
 const models = { User };
--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -5,9 +5,13 @@ const User = require('../../db/models/user');
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const { username } = req.params;
-  const user = await User.findByLogin(username);
-  res.send('Received get request for USER', user);
+  try {
+    const { username } = req.params;
+    const user = await User.findByLogin(username);
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(400);
+  }
 });
 
 module.exports = router;
