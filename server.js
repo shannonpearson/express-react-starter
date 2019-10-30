05c230ebce20cfa7e16b6ce1be7e31b3bc664fc1
shannonpearson <shannon.pearson@goverb.com>
2019-10-30 11:05:11 -0500

    update gitignore and server

--- server.js
+++ server.js
@@ -1,33 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv/config');
-const routes = require('./src/server/routes');
-const path = require('path')
+const path = require('path');
+const bodyParser = require('body-parser');
+const routes = require('./src/server/routes/index');
 
 const { connectDb } = require('./src/db/models/index');
 
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/users', routes.user);
 
-app.get('/test', (req, res) => res.send('Received GET request'));
-
-app.post('/test', (req, res) => res.send('Received POST request'));
-
-app.put('/', (req, res) => res.send('Received PUT request'));
-
-app.delete('/', (req, res) => res.send('Received DELETE request'));
-
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// connectDb().then(async () => {
-app.listen(port, () => console.log(`app listening on port ${port}`));
-// });
+connectDb().then(async () => {
+  console.log('Connected to database');
+  app.listen(port, () => console.log(`app listening on port ${port}`));
+});
